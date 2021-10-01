b1382a4fad795d19883f64d8b085ddff63307fec
tovebr <tove.brandt@student.kyh.se>
2021-10-01 13:52:40 +0200

    first session

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('Lets go');
+
+class Song {
+    constructor(artist, songName, duration) {
+        this.artist = artist;
+        this.songName = songName;
+        this.duration = duration;
+    }
+    
+}
+
+const playList = [];
+
+playList.push(new Song('Britta', 'One more time', 3.23));
+
+console.log(playList);
