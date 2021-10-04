dbc8a9183b428552d473d8f9106d7d1508c65b6f
tovebr <tove.brandt@student.kyh.se>
2021-10-04 09:02:50 +0200

    handle user-input and css added

--- script.js
+++ script.js
@@ -1,15 +1,55 @@
 
+// Testdata
+
+const playlistTest = [];
+playlistTest.push(new Song('Britta', 'One more time', 'pop'));
+playlistTest.push(new Song('Hazelnut hobo', 'Covfef', 'rock'));
+playlistTest.push(new Song('Dagge', 'Greedy thief', 'country'));
+playlistTest.push(new Song('Locomotive Lars', 'chucka cho', 'alternative'));
+playlistTest.push(new Song('Helms deep', 'into the mountain', 'indie'));
+playlistTest.push(new Song('Abracadabra', 'pick a card', 'pop'));
+
+
+//MELI: class Song to create new objects
 class Song {
-    constructor(artist, songName, duration) {
-        this.artist = artist;
-        this.songName = songName;
-        this.duration = duration;
+    constructor(artist, song, genre) {
+      this.artist = artist;
+      this.song = song;
+      this.genre = genre;
     }
-    
-}
+  }
+  
+ const playList = [];
 
-const playList = [];
-
-playList.push(new Song('Britta', 'One more time', 3.23));
-
-console.log(playList);
+  //MELI: Function to activate the script at the click of the button
+  addPlaylist.addEventListener("click", function newSong() {
+    //Collecting values from the input fields
+    let artist = document.getElementById("enterArtist").value;
+    let song = document.getElementById("enterSong").value;
+    let genre = document.getElementById("enterGenre").value;
+  
+    //Displays error message if all input fields haven't been entered
+    if (artist == "" || song == "" || genre == "") {
+      output.innerHTML = "You have to enter all information";
+    } else if (artist != "" && song != "" && genre != "") {
+      output.innerHTML = "You have saved a track to your playlist. Save as many as you like!";
+    }
+  
+    //Pushes in song into array if all input fields have been entered
+    if (artist != "" && song != "" && genre != "") {
+      playList.push(new Song(artist, song, genre));
+      console.log(playList);
+    }
+  });
+  
+  //MELI: Cleans text output with click of a button
+  cleanInput.addEventListener("click", function cleanInput() {
+    document.getElementById("enterArtist").value = "";
+    document.getElementById("enterSong").value = "";
+    document.getElementById("enterGenre").value = "";
+  });
+  
+  //MELI: Cleans text input with click of a button
+  cleanOutput.addEventListener("click", function cleanOutput() {
+    document.getElementById("output").innerHTML = "";
+  });
